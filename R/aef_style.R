3ecde7a595338fc9ed419cbcae7bcf377dc3b195
Pierre <pierre.deroudilhe@protonmail.com>
2023-03-14 11:42:03 +0100

    corrected R code, updated dependencies (added ggtext to soft wrap long lines of text on charts)

--- R/aef_style.R
+++ R/aef_style.R
@@ -3,17 +3,17 @@ aef_style <- function() {
   ggplot2::theme(
     # Text format:
     # This sets the font, size, type and color of text for the chart's title
-    plot.title = ggplot2::element_text(family = font, size = 26, face = "bold", color = "#000000"),
+    plot.title = ggtext::element_textbox_simple(family = font, size = 26, face = "bold", color = "#000000"),
     # This sets the font, size, type and color of text for the chart's subtitle. Also this sets a margin between the title and the subtitle.
-    plot.subtitle = ggplot2::element_text(family = font, size = 18, color = "#000000", margin = ggplot2::margin(8, 0, 8, 0)),
+    plot.subtitle = ggtext::element_textbox_simple(family = font, size = 18, color = "#000000", margin = ggplot2::margin(8, 0, 8, 0)),
     # This sets the font, size, type and color of text for the chart's caption
-    plot.caption = ggplot2::element_text(hjust = 0, family = font, size = 14, face = "italic", color = "#5e6169"),
+    plot.caption = ggtext::element_textbox_simple(hjust = 0, family = font, size = 14, face = "italic", color = "#5e6169"),
     # Legend format:
     # This sets the position and alignment of the legend
     legend.position = "top",
     legend.text.align = 0,
-    # This sets space between legend keys when there are several rows (need guide_legend byrow to be set to TRUE)
-    legend.spacing.y = ggplot2::unit(0.003, "npc"),
+    # This sets space between legend keys when there are several rows (need corresponding legend type guide_legend byrow to be set to TRUE)
+    legend.spacing.y = ggplot2::unit(0.01, "npc"),
     # This removes legend background, title and key
     legend.background = ggplot2::element_blank(),
     legend.title = ggplot2::element_blank(),
@@ -41,6 +41,6 @@ aef_style <- function() {
     # This sets the panel background for facet wrapped plots to white
     strip.background = ggplot2::element_rect(fill = "#ffffff"),
     #This set the size for facet wrapped plots titles
-    strip.text = ggplot2::element_text(family = font, size = 18, color = "#000000", hjust = 0)
+    strip.text = ggplot2::element_text(family = font, size = 14, color = "#000000", hjust = 0)
   )
 }
